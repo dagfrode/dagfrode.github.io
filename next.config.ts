c5fb5e4d0c4ab65d5dcc900c6bec836e91540c57
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-03-08 14:19:23 +0100

    Maybe trailing slashes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
   distDir: "out",
+  trailingSlash: true,
+  exportTrailingSlash: true,
 };
 
 export default nextConfig;
